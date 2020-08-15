b37f9dd1283ca1c7351b53dc2dd8c41adaf27866
Ángel Tapia <41277597+angel-tapia@users.noreply.github.com>
2020-08-15 12:09:34 -0500

    Update Class_Snake.py
    Optimizations of the functions, reducing the lines

--- Game/Class_Snake.py
+++ Game/Class_Snake.py
@@ -6,41 +6,34 @@ class Snake:
         #body[i][0]=x and body[i][1]=y
         self.body=[(screenSize/2,screenSize/2),(screenSize/2,screenSize/2+self.size)]
         self.direction = "Left"
-    def move(self,food):
-        if self.direction is "Left":
-            newhead=(self.body[0][0]-self.speed,self.body[0][1])
-        if self.direction is "Right":
-            newhead=(self.body[0][0]+self.speed,self.body[0][1])
-        if self.direction is "Up":
-            newhead=(self.body[0][0],self.body[0][1]-self.speed)
-        if self.direction is "Down":
-            newhead=(self.body[0][0],self.body[0][1]+self.speed)
+    def move(self, food):
+        mov =[(-self.speed,0),(self.speed,0),(0,-self.speed),(0,self.speed) ]
+        directions = ["Left","Right","Up","Down"]
+        idx=directions.index(self.direction)
+        newhead=(self.body[0][0]+mov[idx][0],self.body[0][1]+mov[idx][1])
         self.body.insert(0,newhead)
         if newhead == food.pos:
             return True
         self.body.pop()
         return False
+
     def valid(self):
         x=self.body[0][0]
         y=self.body[0][1]
-        if x < 0 :
-            return False
-        if x > screenSize-self.size:
-            return False
-        if y < 0 :
-            return False
-        if y > screenSize-self.size:
+        if x < 0 or x > screenSize-self.size or y < 0 or y > screenSize-self.size:
             return False
         head=self.body[0]
         if self.body.count(head) > 1:
             return False
         return True
-    def newdirection(self,direction):
+
+    def newdirection(self, direction):
         directions = ["Left","Up","Right","Down"]
-        indexdir=directions.index(self.direction)
-        indexnewdir=directions.index(direction)
-        if (indexdir+2)%4 is not indexnewdir:
+        indexDir=directions.index(self.direction)
+        indexNewDir=directions.index(direction)
+        if (indexDir+2)%4 is not indexNewDir:
             self.direction=direction
+
 class Food:
     def __init__ (self):
         self.size=10
