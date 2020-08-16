751159760a1ddcbd00bd6aff5fa0ac38ba1848ae
Ángel Tapia <41277597+angel-tapia@users.noreply.github.com>
2020-08-16 12:10:40 -0500

    Update Class_Snake.py

--- Game/Class_Snake.py
+++ Game/Class_Snake.py
@@ -1,48 +1,48 @@
 import pygame
 import random
 screenSize = 500
+directions = ["Left","Up","Right","Down"]
 class Snake:
     def __init__ (self):
-        self.size=10
-        self.speed=10
+        self.size = 10
+        self.speed = 10
         #body[i][0]=x and body[i][1]=y
-        self.body=[(screenSize/2,screenSize/2),(screenSize/2,screenSize/2+self.size)]
+        self.body = [(screenSize/2,screenSize/2)]
         self.direction = "Left"
 
     def move(self, food):
-        mov =[(-self.speed,0),(self.speed,0),(0,-self.speed),(0,self.speed) ]
-        directions = ["Left","Right","Up","Down"]
-        idx=directions.index(self.direction)
-        newhead=(self.body[0][0]+mov[idx][0],self.body[0][1]+mov[idx][1])
-        self.body.insert(0,newhead)
-        if newhead == food.pos:
+        mov = [(-self.speed,0),(0,-self.speed),(self.speed,0),(0,self.speed) ]
+        (x,y)=self.body[0]
+        idx = directions.index(self.direction)
+        (dx,dy)=mov[idx]
+        newHead = (x+dx,y+dy)
+        self.body.insert(0,newHead)
+        if newHead == food.pos:
             return True
         self.body.pop()
         return False
 
     def valid(self):
-        x=self.body[0][0]
-        y=self.body[0][1]
+        (x,y) = self.body[0]
         if x < 0 or x > screenSize-self.size or y < 0 or y > screenSize-self.size:
             return False
-        head=self.body[0]
+        head = self.body[0]
         if self.body.count(head) > 1:
             return False
         return True
 
-    def newdirection(self, direction):
-        directions = ["Left","Up","Right","Down"]
-        indexDir=directions.index(self.direction)
-        indexNewDir=directions.index(direction)
+    def newDirection(self, direction):
+        indexDir = directions.index(self.direction)
+        indexNewDir = directions.index(direction)
         if (indexDir+2)%4 is not indexNewDir:
-            self.direction=direction
+            self.direction = direction
 
 class Food:
     def __init__ (self):
-        self.size=10
-        self.pos=((random.randint(1,49)*10),(random.randint(1,49)*10))
+        self.size = 10
+        self.pos = ((random.randint(1,49)*10),(random.randint(1,49)*10))
 
     def appearFood(self, snake):
          #while the new food is in the body of the snake appear a new food
         while self.pos in snake.body:
-            self.pos=((random.randint(1,49)*10),(random.randint(1,49)*10))
+            self.pos = ((random.randint(1,49)*10),(random.randint(1,49)*10))
