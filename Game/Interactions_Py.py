59f2663d1062af318c74c1c01cdf0e0dd39c401a
Ángel Tapia <41277597+angel-tapia@users.noreply.github.com>
2020-08-16 12:11:07 -0500

    Update Interactions_Py.py

--- Game/Interactions_Py.py
+++ Game/Interactions_Py.py
@@ -1,18 +1,17 @@
 import pygame
 def keyReceived(key,direction):
     if key[pygame.K_DOWN]:
-        direction="Down"
+        direction = "Down"
     if key[pygame.K_UP]:
-        direction="Up"
+        direction = "Up"
     if key[pygame.K_RIGHT]:
-        direction="Right"
+        direction = "Right"
     if key[pygame.K_LEFT]:
-        direction="Left"
+        direction = "Left"
     return direction
 
 def isPause(key,pause):
     if key[pygame.K_p]:
-        pause = True
-    if key[pygame.K_u]:   
-        pause = False
+        pause = not pause
     return pause
+        
