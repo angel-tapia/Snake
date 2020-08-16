a10940a2df4b702f1d223b57122f5e7d8fafc2f3
Ángel Tapia <41277597+angel-tapia@users.noreply.github.com>
2020-08-16 17:15:00 -0500

    Add files via upload

---
+++ Game/Constants.py
@@ -0,0 +1,12 @@
+#Constant variables
+pixelSize = 10
+speed = 10
+foodSize = 10
+screenSize = 500
+directions = ["Left", "Up", "Right", "Down"]
+movMap = { 
+    "Left"  : (-speed, 0),
+    "Up"    : (0, -speed), 
+    "Right" : (speed, 0),
+    "Down"  : (0, speed),
+}
---
+++ Game/main.py
@@ -0,0 +1,12 @@
+import pygame
+import random
+from Constants import *
+from Class_Snake import *
+from Interactions_Py import *
+from Snake_Game import *
+
+if __name__ == '__main__':
+    game = Game()
+    while game.running :
+        game.simulate()
+        game.draw()
